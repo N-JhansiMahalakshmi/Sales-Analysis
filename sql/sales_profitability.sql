363d418a561c5bdd92427005d785aad537856d2c
N Jhansi mahaakshmi <njhansimahalakshmi1522@gmail.com>
2026-04-13 20:59:47 +0530

    Update sales_profitability.sql

--- sql/sales_profitability.sql
+++ sql/sales_profitability.sql
@@ -1,1 +1,49 @@
+-- =============================================
+-- Project  : Sales Analysis
+-- File     : sales_profitability.sql
+-- Author   : N Jhansi mahalakshmi
+-- Date     : April 2026
+-- =============================================
 
+-- -----------------------------------------------
+-- PART 1A: Total Sales by Category
+-- -----------------------------------------------
+SELECT 
+    od.Category,
+    SUM(od.Amount) AS total_sales
+FROM list_of_orders lo
+JOIN order_details od ON lo.`Order ID` = od.`Order ID`
+GROUP BY od.Category
+ORDER BY total_sales DESC;
+
+
+-- -----------------------------------------------
+-- PART 1B: Average Profit and Profit Margin
+-- -----------------------------------------------
+SELECT 
+    od.Category,
+    AVG(od.Profit) AS avg_profit_per_order,
+    ROUND(SUM(od.Profit) / SUM(od.Amount) * 100, 2) AS profit_margin_pct
+FROM list_of_orders lo
+JOIN order_details od ON lo.`Order ID` = od.`Order ID`
+GROUP BY od.Category
+ORDER BY profit_margin_pct DESC;
+
+
+-- -----------------------------------------------
+-- PART 1C: Top and Underperforming Categories
+-- -----------------------------------------------
+SELECT 
+    od.Category,
+    SUM(od.Amount) AS total_sales,
+    AVG(od.Profit) AS avg_profit,
+    ROUND(SUM(od.Profit) / SUM(od.Amount) * 100, 2) AS profit_margin_pct,
+    CASE 
+        WHEN SUM(od.Profit) / SUM(od.Amount) * 100 >= 15 THEN 'Top Performer'
+        WHEN SUM(od.Profit) / SUM(od.Amount) * 100 BETWEEN 5 AND 14 THEN 'Average'
+        ELSE 'Underperforming'
+    END AS performance_status
+FROM list_of_orders lo
+JOIN order_details od ON lo.`Order ID` = od.`Order ID`
+GROUP BY od.Category
+ORDER BY profit_margin_pct DESC;
